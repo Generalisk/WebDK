f9e4b1f11d3b18e2461f123354db1541a521bb78
Generalisk <80116907+Generalisk@users.noreply.github.com>
2025-04-29 10:22:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,9 +15,10 @@
     <body>
         <div id="content">
             <center><img src="content/graphics/titlecard.png" width="85%" /></center>
+            <h4>$(Website_Description)</h4>
+            <hr></hr>
             <h4>$(Home_Description)</h4>
             <hr></hr>
-            <h4>$(Website_Description)</h4>
             <h4>$(IndexPHP_Description)</h4>
         </div>
     </body>
