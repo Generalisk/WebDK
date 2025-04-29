376e1f236e7694c058cad13171da8ef07194b02d
Generalisk <80116907+Generalisk@users.noreply.github.com>
2025-04-29 10:21:46 +0100

    Allow Theme to be overwritten by URL parameter

--- content/scripts/themes.js
+++ content/scripts/themes.js
@@ -1,9 +1,13 @@
 // Called once everything has finished loading
 // Premature loading can cause bugs to occur due to the webinfo document not finished loading yet
 window.addEventListener("load", function() {
-    if (!webinfo.features.includes("cookies")) {
-        console.error("\"Themes\" feature requires feature \"Cookies\" to be enabled!");
-        return;
+    var theme = null;
+
+    // Check if Cookies feature is enabled
+    const cookiesEnabled = webinfo.features.includes("cookies");
+    
+    if (!cookiesEnabled) {
+        console.warn("Some aspects of the \"Themes\" feature requires the \"Cookies\" feature to be enabled!");
     }
 
     // Load Theme Style Sheets
@@ -14,11 +18,29 @@ window.addEventListener("load", function() {
         document.head.appendChild(y);
     });
 
-    // Set Default Theme if not set yet
-    if (GetCookie("theme") === null || GetCookie("theme") === undefined) {
-        SetCookie("theme", webinfo.defaultTheme);
+    if (GetParameter("theme") === null || GetParameter("theme") === undefined) {
+        // Terminate script here if cookies feature isn't enabled
+        if (!cookiesEnabled) {
+            return;
+        }
+    
+        // Set Default Theme if not set yet
+        if (GetCookie("theme") === null || GetCookie("theme") === undefined) {
+            SetCookie("theme", webinfo.defaultTheme);
+        }
+
+        // Set active theme
+        theme = GetCookie("theme");
+    } else {
+        // Update theme to paramater
+        theme = GetParameter("theme");
+
+        // Set cookie to the ovewritten parameter
+        if (cookiesEnabled) {
+            SetCookie("theme", theme);
+        }
     }
 
     // Apply theme
-    document.documentElement.classList.add("theme-" + GetCookie("theme"));
+    document.documentElement.classList.add("theme-" + theme);
 }, false);
